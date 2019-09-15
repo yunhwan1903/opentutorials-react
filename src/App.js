d8abdcd0606a412d231d31da28504bea5791a7d6
yun <yunhwan1903@gmail.com>
2019-09-15 17:56:56 +0900

    [update] component 만들기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Subject extends Component {
+
+    render(){
+        return (
+            <header>
+                <h1>WEB</h1>
+                word wide web !
+            </header>
+        );
+    }
+}
+
+class Subject2 extends Component {
+    render(){
+        return (
+                <nav>
+                    <ul>
+                    <li><a href="1.html">HTML</a></li>
+                    <li><a href="2.html">CSS</a></li>
+                    <li><a href="3.html">JavaScript</a></li>
+                    </ul>
+                </nav>
+        );
+    }
+}
+
+class Content extends Component {
+    render(){
+        return (
+                <article>
+                    <h2>HTML</h2>
+                    HTML is HyperText Markup Language.
+                </article>
+        );
+    }
+}
 class App extends Component {
 
     render() {
         return (
             <div className="App">
-                "Hello, React!!"
+                <Subject></Subject>
+                <Subject2></Subject2>
+                <Content></Content>
             </div>
+
         );
     }
 }
+
+
 
 //function App() {
 //  return (
